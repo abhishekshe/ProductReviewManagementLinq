573a0e96641fe07c18be73ae29d3723b9a1f59ba
Abhishe <abhisheknarwariya813@gmail.com>
2022-04-22 14:13:40 +0530

    Added Table

--- ProductReviewManagement/LinqToDataTable.cs
+++ ProductReviewManagement/LinqToDataTable.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ProductReviewManagementWithLinq
+namespace ProductReviewManagement
 {
-    internal class LinqToDataTable
+    class LinqToDataTable
     {
+        public void AddDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Id");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Chai");
+            table.Rows.Add("2", "Queso Cabrales");
+            table.Rows.Add("3", "Tofu");
+            DisplayProducts(table);
+        }
+        public void DisplayProducts(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("ProductNames:");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
     }
 }
